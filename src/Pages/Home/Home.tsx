5c4e0841e5a4df4cb4ceba78312dd28948f6d07d
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-24 00:00:58 +0330

    fix(Pages/Home): Add axios api

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Product from '../../Components/Product/Product'
 function Home() {
+  const [products , setProducts] = useState([])
+
+  useEffect(()=>{
+    axios.post("https://imis.silverage.co/api/shop/products/search" , {
+        search_keys : "",
+        group_depth : 1,
+        load_products : 1,
+        load_group : 1,
+        per_page : 10
+    }).then(res => setProducts(res.data.results.products))
+  } , [])
+
   return (
-    <div>Home</div>
+    <div className="home-page">
+      {
+        products.map(product => (
+          <>
+          <Product {...product}/>
+          </>
+        ))
+      }
+    </div>
   )
 }
 
