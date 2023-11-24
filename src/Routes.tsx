8c5cf3817943d02f6cb9fddd1c559e080b4fb84e
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-25 00:25:32 +0330

    fix(root/Routes): fix all of routes configs

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,24 @@
+import About from "./Pages/About/About";
+import Cart from "./Pages/Cart/Cart";
 import Home from "./Pages/Home/Home";
+import SingleProduct from "./Pages/SingleProduct/SingleProduct";
 
 let pageRoutes = [
     {
         path:"/" , 
         element:<Home />
+    },
+    {
+        path:"/cart",
+        element:<Cart />
+    },
+    {
+        path:"/about",
+        element:<About />
+    },
+    {
+        path:"/product/:id",
+        element:<SingleProduct />
     }
 ]
 export default pageRoutes
