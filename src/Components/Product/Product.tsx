fece1ac16519d67399c7b4876ac7be2943687c56
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-24 22:14:30 +0330

    fix(Components/Product)

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./Product.css"
-function Product({name , cover}) {
+import { Link } from 'react-router-dom'
+function Product(props:{name:string , count_in_box:number  , count_unit:object , full_price:{sale_price_with_tax_and_discount : number} , cover:string}) {
   return (
+    <Link to="/" className='product-link'>
+    
     <div className='product'>
-      <img src={cover} alt='product image'/>
-      <h3>{name}</h3>
+      <img src={props.cover} alt='product image'/>
+      <div className="product-body">
+
+      <h3>{props.name}</h3>
+      <p className='product-price'>{props.full_price.sale_price_with_tax_and_discount} تومان</p>
+
+      <p className='product-count'>{props.count_in_box >= 0 ? props.count_in_box + "عدد" : "موجود نیست"}</p>
+      </div>
     </div>
+    </Link>
   )
 }
 
