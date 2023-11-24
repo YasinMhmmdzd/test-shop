b122b05c96eabe9a69c1f3e52f5458dbff175a33
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-24 22:58:28 +0330

    fix(Pages/Home):Add footer to home page and fix styles

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { HiHome } from "react-icons/hi";
+import "./Footer.css"
+import { HiHome  , HiUser , HiShoppingCart} from "react-icons/hi";
+import { Link, useLocation } from 'react-router-dom';
 function Footer() {
+    const current_route = useLocation().pathname
   return (
     <footer>
+        <Link to="/" className={current_route === "/" ? 'active-route' : ''}>
         <HiHome />
+        <p>خانه</p>
+        </Link>
+        <Link to="/cart" className={current_route === "/cart" ? 'active-route' : ''}>
+        <HiShoppingCart />
+        <p>سبد خرید</p>
+        </Link>
+        <Link to="/about" className={current_route === "/about" ? 'active-route' : ''}>
+        <HiUser />
+        <p>درباره ما</p>
+        </Link>
     </footer>
   )
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -5,20 +5,28 @@ import "./Home.css"
 
 function Home() {
   const [products , setProducts] = useState([])
+  const [isFetchPending , setIsFetchPending] = useState(false)
   useEffect(()=>{
+    setIsFetchPending(true)
     axios.post("https://imis.silverage.co/api/shop/products/search" , {
       seach_key : "",
       group_depth : 1,
       load_products : 1 ,
       load_group : 1,
       per_page : 12
-    }).then(res => setProducts(res.data.results.products))
+    }).then(res => {
+      setProducts(res.data.results.products)
+      setIsFetchPending(false)
+    })
   } , [])
 
   return (
 
     <div className="home-page">
       <h2>محصولات</h2>
+      {isFetchPending && (
+        <p className="loading">درحال بارگزاری ...</p>
+      )}
       <div className="products-container">
 
       {products.map(product => (
