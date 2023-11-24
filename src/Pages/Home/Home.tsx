65a2193b13a084ed87b9b95417e1e1c63e29c5ba
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-24 22:16:47 +0330

    fix(Pages/Home): Add footer to Home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Product from '../../Components/Product/Product'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Product from "../../Components/Product/Product"
+import "./Home.css"
+
 function Home() {
   const [products , setProducts] = useState([])
-
   useEffect(()=>{
     axios.post("https://imis.silverage.co/api/shop/products/search" , {
-        search_keys : "",
-        group_depth : 1,
-        load_products : 1,
-        load_group : 1,
-        per_page : 10
+      seach_key : "",
+      group_depth : 1,
+      load_products : 1 ,
+      load_group : 1,
+      per_page : 12
     }).then(res => setProducts(res.data.results.products))
   } , [])
 
   return (
+
     <div className="home-page">
-      {
-        products.map(product => (
-          <>
-          <Product {...product}/>
-          </>
-        ))
-      }
+      <h2>محصولات</h2>
+      <div className="products-container">
+
+      {products.map(product => (
+        
+        <Product key={product.id}  {...product as Object} />
+        
+      ))}
+
+      </div>
     </div>
+
   )
 }
 
