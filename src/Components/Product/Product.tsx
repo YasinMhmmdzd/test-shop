f98bc447784440df6c75036804bc911d4ecff696
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-25 00:27:22 +0330

    fix(Components/Product): Add href to links

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {HiShoppingCart} from "react-icons/hi"
 import "./Product.css"
 import { Link } from 'react-router-dom'
-function Product(props:{name:string , count_in_box:number  , count_unit:object , full_price:{sale_price_with_tax_and_discount : number} , cover:string}) {
+function Product(props:{name:string , count_in_box:number  , count_unit:object , full_price:{sale_price_with_tax_and_discount : number} , cover:string , slug:string}) {
   return (
-    <Link to="/" className='product-link'>
+    <Link to={`/product/${props.slug}`} className='product-link'>
     
     <div className='product'>
       <img src={props.cover} alt='product image'/>
@@ -13,6 +14,7 @@ function Product(props:{name:string , count_in_box:number  , count_unit:object ,
       <p className='product-price'>{props.full_price.sale_price_with_tax_and_discount} تومان</p>
 
       <p className='product-count'>{props.count_in_box >= 0 ? props.count_in_box + "عدد" : "موجود نیست"}</p>
+      <button className='add-to-cart-btn'> <HiShoppingCart size={22}/> افزودن به سبد خرید</button>
       </div>
     </div>
     </Link>
