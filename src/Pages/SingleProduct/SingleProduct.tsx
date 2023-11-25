4714f7553275c44bc25c09287c2b07cb02eac6c6
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-25 18:33:58 +0330

    fix(Components/SingleProduct): fix logics

--- src/Pages/SingleProduct/SingleProduct.tsx
+++ src/Pages/SingleProduct/SingleProduct.tsx
@@ -4,28 +4,50 @@ import { useParams } from 'react-router-dom'
 
 function SingleProduct() {
   const currentRoute = useParams().id
-  
-  
-  
   const [productDetails , setProductDetails] : any = useState({})
+  const [isLoading , setIsLoading] = useState(true)
 
   useEffect(()=>{
     axios.post("https://imis.silverage.co/api/shop/products/details" , {
       id : currentRoute
-    }).then(res => {setProductDetails(res.data.results.product)})
+    }).then(res => {
+      setProductDetails(res.data.results.product)
+      setIsLoading(false)
+    })
   } , [])
 
-  console.log(productDetails);
-  
 
   return (
     <div className='single-product'>
-      <h2>
-        {productDetails.name}
-      </h2>
+      {isLoading ? <p className='loading'>درحال بارگزاری...</p> : (
+        <>
       <div className="single-product-image">
         <img src={productDetails.cover} alt="cover product" />
       </div>
+
+      <div className="single-proudct-infos">
+      <h2>
+        {productDetails.name}
+      </h2>
+      <p>
+      {productDetails.count_in_box >= 0 ? productDetails.count_in_box + "" + productDetails.count_unit.title : "موجود نیست"}
+      </p>
+      <p>
+      {productDetails.full_price.sale_price_with_tax_and_discount} تومان
+      </p>
+
+      </div>
+      <div className="single-product-description">
+        <h5>
+          {productDetails.brief_description}
+        </h5>
+        <p>
+          {productDetails.description}
+        </p>
+      </div>
+        
+        </>
+      )}
     </div>
   )
 }
