682f38b9e845df521f7e088e88576b74db9cfd41
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-11-26 22:54:30 +0330

    fix(Pages/Cart): cart logic

--- src/Pages/Cart/Cart.tsx
+++ src/Pages/Cart/Cart.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { HiShoppingBag } from "react-icons/hi";
+import CartProduct from '../../Components/CartProduct/CartProduct';
+import "./Cart.css"
 
 function Cart() {
+  let cartProducts = localStorage.getItem("cartProduct")
+  const [isLoading , setIsLoading] = useState(true)
+  const [allProducts , setAllProducts] = useState([])
+  useEffect(()=>{
+
+    axios.post("https://imis.silverage.co/api/shop/cart/details" , {
+      products : cartProducts
+    }).then(res => {
+      console.log(res)
+      setAllProducts(res.data.results.cart)
+      setIsLoading(false)
+    })
+  } , [])
   return (
-    <div>Cart</div>
+    <div className="cart">
+      {isLoading ? <p className='loading'>درحال بارگزاری ... </p> :
+      <div className="cart-container">
+       {
+        allProducts.items.length <= 0 ? <p style={{textAlign:'center' , padding:'20px'}}>سبد خرید خالی است</p> :(
+          allProducts.items.map(product => (
+            <CartProduct {...product}/>
+          ))
+        )
+       }
+       <div className="payable-cart">
+
+      <h4>
+        مجموع سبد خرید :‌
+      </h4>
+      <div className="payable">
+      <HiShoppingBag className="pay-icon"/>
+      {allProducts.payable} تومان
+      </div>
+
+       </div>
+
+      </div>
+      }
+    </div>
   )
 }
 
